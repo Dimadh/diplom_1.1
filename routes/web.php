9b57eff1394828ab5196969403440e1fcf3b626e
Dimadg <hanzha1995@mail.ru>
2017-05-17 17:21:40 +0300

    first commit()

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,14 @@
     return view('welcome');
 });*/
 
-Route::get('/', ['uses' => 'HomeController@index' , 'as' =>'home']);
-Route::post('/', 'HomeController@storer')->name('programmer');
-Route::post('/', 'HomeController@storers')->name('task');
-Route::get('/autorisation', ['uses' => 'HomeController@autorisation' , 'as' =>'home1']);
+Route::get('/', ['uses' => 'HomeController@autorisation' , 'as' =>'home1']);
+Route::get('/autorisation', ['uses' => 'HomeController@index' , 'as' =>'home']);
+Route::post('/autorisation', 'HomeController@storer')->name('programmer');
+Route::post('/autorisation', 'HomeController@storers')->name('task');
 Route::get('/registration', ['uses' => 'HomeController@registration' , 'as' => 'home2']);
 Route::post('/registration', 'HomeController@store')->name('customer');
 Route::get('/task', ['uses' => 'HomeController@mainform' , 'as' => 'home3']);
-Route::post('/take_prog', ['uses' => 'HomeController@search' , 'as' => 'issue']);
+Route::post('/take_prog', ['uses' => 'HomeController@addProgrammer' , 'as' => 'issue']);
 Route::post('/send_task', ['uses' => 'HomeController@send' , 'as' => 'issue1']);
 Route::post('/update_task', ['uses' => 'HomeController@updatetask' , 'as' => 'issue2']);
 Route::post('/create_task', ['uses' => 'HomeController@createAndsend' , 'as' => 'issue3']);
